e53bde2acafdf3be99c145dda1b15ea1a7c087cb
dira sanjaya wardana <dirawardana.dw@gmail.com>
2024-04-29 08:42:39 +0700

    Initial Commit

---
+++ Animalia/AnimaliaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimaliaApp.swift
+//  Animalia
+//
+//  Created by MACBOOK PRO on 29/04/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimaliaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
